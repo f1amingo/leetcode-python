883416e4db225b434be5234649058945b4231b5a
flamingo <zsjperiod@foxmail.com>
2019-09-16 13:16:08 +0800

    33. Search in Rotated Sorted Array 直接二分

--- hot-100/medium/33. Search in Rotated Sorted Array.py
+++ hot-100/medium/33. Search in Rotated Sorted Array.py
@@ -1,6 +1,6 @@
 class Solution(object):
     # 直觉办法 先找pivot
-    def search(self, nums, target):
+    def search0(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
@@ -34,5 +34,30 @@ class Solution(object):
                 hi = mid - 1
         return -1
 
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[l] == target:
+                return l
+            if nums[r] == target:
+                return r
+            if nums[l] < nums[m]:
+                if nums[l] < target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] < target < nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return -1
 
-print(Solution().search([1, 3], 3))
+
+print(Solution().search([3, 1], 1))
