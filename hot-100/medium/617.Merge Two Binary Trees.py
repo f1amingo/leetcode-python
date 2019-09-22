2d29d255946317f94c94d449831801284d484f49
flamingo <zsjperiod@foxmail.com>
2019-09-22 10:27:22 +0800

    merge tree 简化代码

--- hot-100/medium/617.Merge Two Binary Trees.py
+++ hot-100/medium/617.Merge Two Binary Trees.py
@@ -7,27 +7,37 @@ class TreeNode(object):
 
 
 class Solution(object):
+    # def mergeTrees(self, t1, t2):
+    #     """
+    #     :type t1: TreeNode
+    #     :type t2: TreeNode
+    #     :rtype: TreeNode
+    #     """
+    #     root = TreeNode(0)
+    #     if not t1 and not t2:
+    #         return None
+    #     if t1 and not t2:
+    #         root.val = t1.val
+    #         root.left = self.mergeTrees(t1.left, t2)
+    #         root.right = self.mergeTrees(t1.right, t2)
+    #     elif t2 and not t1:
+    #         root.val = t2.val
+    #         root.left = self.mergeTrees(t1, t2.left)
+    #         root.right = self.mergeTrees(t1, t2.right)
+    #     else:
+    #         root.val = t1.val + t2.val
+    #         root.left = self.mergeTrees(t1.left, t2.left)
+    #         root.right = self.mergeTrees(t1.right, t2.right)
+    #     return root
+
     def mergeTrees(self, t1, t2):
-        """
-        :type t1: TreeNode
-        :type t2: TreeNode
-        :rtype: TreeNode
-        """
-        root = TreeNode(0)
-        if not t1 and not t2:
-            return None
-        if t1 and not t2:
-            root.val = t1.val
-            root.left = self.mergeTrees(t1.left, t2)
-            root.right = self.mergeTrees(t1.right, t2)
-        elif t2 and not t1:
-            root.val = t2.val
-            root.left = self.mergeTrees(t1, t2.left)
-            root.right = self.mergeTrees(t1, t2.right)
-        else:
-            root.val = t1.val + t2.val
-            root.left = self.mergeTrees(t1.left, t2.left)
-            root.right = self.mergeTrees(t1.right, t2.right)
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+        root = TreeNode(t1.val + t2.val)
+        root.left = self.mergeTrees(t1.left, t2.left)
+        root.right = self.mergeTrees(t1.right, t2.right)
         return root
 
 
