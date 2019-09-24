be1a0310d5bee8bdfa50edfabfeefe50c3bf3ad9
flamingo <zsjperiod@foxmail.com>
2019-09-24 17:01:20 +0800

    子集 dfs

--- hot-100/medium/78. Subsets.py
+++ hot-100/medium/78. Subsets.py
@@ -1,15 +1,26 @@
 class Solution(object):
+    # def subsets(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: List[List[int]]
+    #     """
+    #     res = []
+    #     for (i, num) in enumerate(nums):
+    #         for clist in res[:]:
+    #             res.append(clist + [num])
+    #         res.append([num])
+    #     res.append([])
+    #     return res
+
     def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
         res = []
-        for (i, num) in enumerate(nums):
-            for clist in res[:]:
-                res.append(clist + [num])
-            res.append([num])
-        res.append([])
+
+        def dfs(nums, index, path):
+            res.append(path)
+            for i in range(index, len(nums)):
+                dfs(nums, i + 1, path + [nums[i]])
+
+        dfs(nums, 0, [])
         return res
 
 
