bbfcdf79a10d7baa1d06e39fc5be360bc69a63db
flamingo <zsjperiod@foxmail.com>
2019-09-25 13:36:27 +0800

    相交链表 链表拼接

--- hot-100/easy/160. 相交链表.py
+++ hot-100/easy/160. 相交链表.py
@@ -8,28 +8,37 @@ class Solution(object):
 
     # 先遍历获取长度
     # 长链表先走长度差
+    # def getIntersectionNode(self, headA, headB):
+    #     if not (headA and headB):
+    #         return None
+    #
+    #     def list_size(head):
+    #         size = 0
+    #         while head:
+    #             size += 1
+    #             head = head.next
+    #         return size
+    #
+    #     size1, size2 = list_size(headA), list_size(headB)
+    #     ptr1, ptr2 = headA, headB
+    #     if size1 > size2:
+    #         for i in range(size1 - size2):
+    #             ptr1 = ptr1.next
+    #     elif size2 > size1:
+    #         for i in range(size2 - size1):
+    #             ptr2 = ptr2.next
+    #     while ptr1 and ptr2:
+    #         if ptr1 == ptr2:
+    #             return ptr1
+    #         ptr1 = ptr1.next
+    #         ptr2 = ptr2.next
+    #     return None
+
     def getIntersectionNode(self, headA, headB):
-        if not (headA and headB):
+        if not headA and not headB:
             return None
-
-        def list_size(head):
-            size = 0
-            while head:
-                size += 1
-                head = head.next
-            return size
-
-        size1, size2 = list_size(headA), list_size(headB)
-        ptr1, ptr2 = headA, headB
-        if size1 > size2:
-            for i in range(size1 - size2):
-                ptr1 = ptr1.next
-        elif size2 > size1:
-            for i in range(size2 - size1):
-                ptr2 = ptr2.next
-        while ptr1 and ptr2:
-            if ptr1 == ptr2:
-                return ptr1
-            ptr1 = ptr1.next
-            ptr2 = ptr2.next
-        return None
+        p1, p2 = headA, headB
+        while p1 != p2:
+            p1 = p1.next if p1 else headB
+            p2 = p2.next if p2 else headA
+        return p1
