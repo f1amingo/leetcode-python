352280d0a2de7a2d8d85eec283f206ab627cbb7c
flamingo <zsjperiod@foxmail.com>
2019-10-26 18:32:22 +0800

    删除链表节点

---
+++ easy/删除链表中的节点.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+node1 = ListNode(4)
+node2 = ListNode(5)
+node3 = ListNode(1)
+node4 = ListNode(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+Solution().deleteNode(node2)
+
+a = 1
