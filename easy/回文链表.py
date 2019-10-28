62e8c071c48d8e24a36f69e9102565524a377176
flamingo <zsjperiod@foxmail.com>
2019-10-28 20:05:10 +0800

    回文链表 转换为数组 极简代码

--- easy/回文链表.py
+++ easy/回文链表.py
@@ -7,26 +7,33 @@ class ListNode(object):
 
 class Solution(object):
 
-    # 转换为数组比较
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        if not head:
-            return True
+        # 转换为数组比较
+        # if not head:
+        #     return True
+        # _list = []
+        # while head:
+        #     _list.append(head.val)
+        #     head = head.next
+        # p1 = 0
+        # p2 = len(_list) - 1
+        # while p1 < p2:
+        #     if _list[p1] != _list[p2]:
+        #         return False
+        #     p1 += 1
+        #     p2 -= 1
+        # return True
+
+        # 转换为数组 极简代码
         _list = []
         while head:
             _list.append(head.val)
             head = head.next
-        p1 = 0
-        p2 = len(_list) - 1
-        while p1 < p2:
-            if _list[p1] != _list[p2]:
-                return False
-            p1 += 1
-            p2 -= 1
-        return True
+        return _list == _list[::-1]
 
 
 node1 = ListNode(1)
