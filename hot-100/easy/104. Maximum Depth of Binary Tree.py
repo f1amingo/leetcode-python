b321dbdbeaadcf2a0a97a33021702679562117a2
flamingo <zsjperiod@foxmail.com>
2019-10-30 15:16:47 +0800

    Maximum Depth of Biary Tree 使用栈

--- hot-100/easy/104. Maximum Depth of Binary Tree.py
+++ hot-100/easy/104. Maximum Depth of Binary Tree.py
@@ -12,15 +12,37 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        # my solution
+        # def dfs(_root, _depth):
+        #     if not _root:
+        #         return _depth
+        #     _depth += 1
+        #     _depth = max(dfs(_root.left, _depth), dfs(_root.right, _depth))
+        #     return _depth
+        #
+        # return dfs(root, 0)
 
-        def dfs(_root, _depth):
-            if not _root:
-                return _depth
-            _depth += 1
-            _depth = max(dfs(_root.left, _depth), dfs(_root.right, _depth))
-            return _depth
+        # 参考的解法
+        # if not root:
+        #     return 0
+        # left_height = self.maxDepth(root.left)
+        # right_height = self.maxDepth(root.right)
+        # return max(left_height, right_height) + 1
 
-        return dfs(root, 0)
+        # 栈的解法
+        if not root:
+            return 0
+        max_height = 1
+        stk = [(root, 1)]
+        while stk:
+            _root, height = stk.pop()
+            if height > max_height:
+                max_height = height
+            if _root.left:
+                stk.append((_root.left, height + 1))
+            if _root.right:
+                stk.append((_root.right, height + 1))
+        return max_height
 
 
 root = TreeNode(3)
