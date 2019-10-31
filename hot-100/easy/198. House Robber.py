10a740f9d72fc48317ffaa12edd6389948ea1394
flamingo <zsjperiod@foxmail.com>
2019-10-31 18:51:56 +0800

    house robber 参考后修改

--- hot-100/easy/198. House Robber.py
+++ hot-100/easy/198. House Robber.py
@@ -5,21 +5,34 @@ class Solution(object):
         :rtype: int
         """
         # 我的解法
-        if not nums:
-            return 0
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        dp = [0] * n
-        for i in range(n):
-            if i == 0 or i == 1:
-                dp[i] = nums[i]
-            else:
-                if i - 2 > 0:
-                    dp[i] = nums[i] + max(dp[i - 2], dp[i - 3])
-                else:
-                    dp[i] = nums[i] + dp[i - 2]
-        return max(dp[-1], dp[-2])
+        # if not nums:
+        #     return 0
+        # n = len(nums)
+        # if n == 1:
+        #     return nums[0]
+        # dp = [0] * n
+        # for i in range(n):
+        #     if i == 0 or i == 1:
+        #         dp[i] = nums[i]
+        #     else:
+        #         if i - 2 > 0:
+        #             dp[i] = nums[i] + max(dp[i - 2], dp[i - 3])
+        #         else:
+        #             dp[i] = nums[i] + dp[i - 2]
+        # return max(dp[-1], dp[-2])
+
+        # 参考后修改
+        # if not nums:
+        #     return 0
+        # n = len(nums)
+        # dp = [0] * n
+        # dp[0] = nums[0]
+        # if n > 1:
+        #     dp[1] = max(nums[0], nums[1])
+        # for i in range(2, n):
+        #     dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+        # return dp[-1]
 
 
-print(Solution().rob([6, 8, 1]))
+
+print(Solution().rob([1, 2, 3]))
