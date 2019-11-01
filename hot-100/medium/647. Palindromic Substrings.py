fbe296ef7f65e8e45ba7e0b77e75a72a5d086d4f
flamingo <zsjperiod@foxmail.com>
2019-11-01 12:02:44 +0800

    中心扩散

--- hot-100/medium/647. Palindromic Substrings.py
+++ hot-100/medium/647. Palindromic Substrings.py
@@ -4,18 +4,38 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        result = 0
-        n = len(s)
-        dp = [[0] * n for _ in range(n)]
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                if j - i > 2:
-                    dp[i][j] = s[i] == s[j] and dp[i + 1][j - 1]
-                else:
-                    dp[i][j] = s[i] == s[j]
-                if dp[i][j]:
-                    result += 1
-        return result
+
+        # 动态规划
+        # result = 0
+        # n = len(s)
+        # dp = [[0] * n for _ in range(n)]
+        # for i in range(n - 1, -1, -1):
+        #     for j in range(i, n):
+        #         if j - i > 2:
+        #             dp[i][j] = s[i] == s[j] and dp[i + 1][j - 1]
+        #         else:
+        #             dp[i][j] = s[i] == s[j]
+        #         if dp[i][j]:
+        #             result += 1
+        # return result
+
+        # 中心扩散
+        res = 0
+
+        def helper(left, right):
+            count = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+                count += 1
+            return count
+
+        for i in range(len(s)):
+            # 以s[i]为中心的字符串是否是回文串，奇数
+            res += helper(i, i)
+            # 以s[i]，s[i+1]为中心的字符串是否是回文串，偶数
+            res += helper(i, i + 1)
+        return res
 
 
 # print(Solution().countSubstrings("abc"))
