2a1aecade9730ab99d5feb711bc4b8f3aa4f8cb2
flamingo <zsjperiod@foxmail.com>
2019-11-03 13:13:37 +0800

    数字做下标反号

--- hot-100/easy/448. Find All Numbers Disappeared in an Array.py
+++ hot-100/easy/448. Find All Numbers Disappeared in an Array.py
@@ -16,17 +16,24 @@ class Solution(object):
         # return res
 
         # 鸽巢做法
-        def swap(nums, i1, i2):
-            nums[i1] = nums[i1] ^ nums[i2]
-            nums[i2] = nums[i1] ^ nums[i2]
-            nums[i1] = nums[i1] ^ nums[i2]
+        # def swap(nums, i1, i2):
+        #     nums[i1] = nums[i1] ^ nums[i2]
+        #     nums[i2] = nums[i1] ^ nums[i2]
+        #     nums[i1] = nums[i1] ^ nums[i2]
+        #
+        # n = len(nums)
+        #
+        # for i in range(n):
+        #     while nums[nums[i] - 1] != nums[i]:
+        #         swap(nums, i, nums[i] - 1)
+        # return [i + 1 for i in range(len(nums)) if nums[i] != i + 1]
 
-        n = len(nums)
-
-        for i in range(n):
-            while nums[nums[i] - 1] != nums[i]:
-                swap(nums, i, nums[i] - 1)
-        return [i + 1 for i in range(len(nums)) if nums[i] != i + 1]
+        # 数字做下标反号
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
 
 
 print(Solution().findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
