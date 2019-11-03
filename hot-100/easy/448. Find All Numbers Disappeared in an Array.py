709dc025fc31d52a9e4c5be6e9c5255d84a46dca
flamingo <zsjperiod@foxmail.com>
2019-11-03 13:06:07 +0800

    鸽巢方法

--- hot-100/easy/448. Find All Numbers Disappeared in an Array.py
+++ hot-100/easy/448. Find All Numbers Disappeared in an Array.py
@@ -4,15 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+
         # using counter
-        count = [0] * len(nums)
-        for i in nums:
-            count[i - 1] += 1
-        res = []
-        for i in range(len(nums)):
-            if count[i] == 0:
-                res.append(i + 1)
-        return res
+        # count = [0] * len(nums)
+        # for i in nums:
+        #     count[i - 1] += 1
+        # res = []
+        # for i in range(len(nums)):
+        #     if count[i] == 0:
+        #         res.append(i + 1)
+        # return res
+
+        # 鸽巢做法
+        def swap(nums, i1, i2):
+            nums[i1] = nums[i1] ^ nums[i2]
+            nums[i2] = nums[i1] ^ nums[i2]
+            nums[i1] = nums[i1] ^ nums[i2]
+
+        n = len(nums)
+
+        for i in range(n):
+            while nums[nums[i] - 1] != nums[i]:
+                swap(nums, i, nums[i] - 1)
+        return [i + 1 for i in range(len(nums)) if nums[i] != i + 1]
 
 
 print(Solution().findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
