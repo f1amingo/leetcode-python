8dd79060a4a9e0dd420e4050050f6add5b0b0271
flamingo <zsjperiod@foxmail.com>
2019-11-12 16:15:01 +0800

    官方题解

--- hot-100/medium/49. Group Anagrams.py
+++ hot-100/medium/49. Group Anagrams.py
@@ -15,17 +15,27 @@ class Solution(object):
         # return [res[key] for key in res]
 
         # 统计出现次数确定异位词
-        res = {}
-        for string in strs:
-            statis = [0] * 26
-            for ch in string:
-                statis[ord(ch) - ord('a')] += 1
-            tuple_key = tuple(statis)
-            if tuple_key in res:
-                res[tuple_key].append(string)
-            else:
-                res[tuple_key] = [string]
-        return [val for key, val in res.items()]
+        # res = {}
+        # for string in strs:
+        #     statis = [0] * 26
+        #     for ch in string:
+        #         statis[ord(ch) - ord('a')] += 1
+        #     tuple_key = tuple(statis)
+        #     if tuple_key in res:
+        #         res[tuple_key].append(string)
+        #     else:
+        #         res[tuple_key] = [string]
+        # return [val for key, val in res.items()]
+
+        # 官方题解
+        import collections
+        ans = collections.defaultdict(list)
+        for s in strs:
+            counts = [0] * 26
+            for c in s:
+                counts[ord(c) - ord('a')] += 1
+            ans[tuple(counts)].append(s)
+        return ans.values()
 
 
 print(Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
