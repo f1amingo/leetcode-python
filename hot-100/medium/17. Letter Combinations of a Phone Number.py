99f510e8133b18ac5554f2fd9e26d8f47e80620c
flamingo <zsjperiod@foxmail.com>
2019-11-15 13:14:56 +0800

    电话号码字母组合

--- hot-100/medium/17. Letter Combinations of a Phone Number.py
+++ hot-100/medium/17. Letter Combinations of a Phone Number.py
@@ -1,25 +1,49 @@
+from typing import List
+
+
 class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+    def letterCombinations(self, digits: str) -> List[str]:
+        # if not digits:
+        #     return []
+        # table = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+        # res = []
+        #
+        # def dfs(_digits, cur_set):
+        #     if not _digits:
+        #         res.append(cur_set)
+        #         return
+        #     letters = table[int(_digits[0]) - 2]
+        #     for ch in letters:
+        #         dfs(_digits[1:], cur_set + ch)
+        #
+        # dfs(digits, '')
+        #
+        # return res
+
+        letter_table = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        ans = []
         if not digits:
-            return []
-        table = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-        res = []
+            return ans
 
-        def dfs(_digits, cur_set):
+        def dfs(cur, _digits):
             if not _digits:
-                res.append(cur_set)
+                ans.append(cur)
                 return
-            letters = table[int(_digits[0]) - 2]
-            for ch in letters:
-                dfs(_digits[1:], cur_set + ch)
+            letters = letter_table[_digits[0]]
+            for letter in letters:
+                dfs(cur + letter, _digits[1:])
 
-        dfs(digits, '')
-
-        return res
+        dfs('', digits)
+        return ans
 
 
 print(Solution().letterCombinations("234"))
--- script.py
+++ script.py
@@ -1,3 +1,33 @@
+from typing import List
+
+
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        pass
+    def letterCombinations(self, digits: str) -> List[str]:
+        letter_table = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        ans = []
+        if not digits:
+            return ans
+
+        def dfs(cur, _digits):
+            if not _digits:
+                ans.append(cur)
+                return
+            letters = letter_table[_digits[0]]
+            for letter in letters:
+                dfs(cur + letter, _digits[1:])
+
+        dfs('', digits)
+        return ans
+
+
+print(Solution().letterCombinations(''))
+print(Solution().letterCombinations("23"))
