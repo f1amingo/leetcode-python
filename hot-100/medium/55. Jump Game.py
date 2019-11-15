614826e295ba969190fc9576e4ae3eb1f19958e8
flamingo <zsjperiod@foxmail.com>
2019-11-15 12:56:42 +0800

    跳跃游戏 最远能跳到的位置

--- hot-100/medium/55. Jump Game.py
+++ hot-100/medium/55. Jump Game.py
@@ -21,17 +21,43 @@ class Solution:
 
         # 动态规划
         # 超时
-        if not nums:
-            return True
+        # if not nums:
+        #     return True
+        # n = len(nums)
+        # dp = [False] * n
+        # dp[-1] = True
+        # for i in range(n - 2, -1, -1):
+        #     for step in range(nums[i]):
+        #         if dp[i + step + 1]:
+        #             dp[i] = True
+        #             break
+        # return dp[0]
+
+        # 优化状态转移方程
+        # 超时
+        # if not nums:
+        #     return True
+        # n = len(nums)
+        # dp = [False] * n
+        # dp[-1] = True
+        # for i in range(n - 2, -1, -1):
+        #     end_include = min(i + nums[i], n - 1)
+        #     for j in range(i + 1, end_include + 1):
+        #         if dp[j]:
+        #             dp[i] = True
+        #             break
+        # return dp[0]
+
+        # 考虑最远能跳到的位置
         n = len(nums)
-        dp = [False] * n
-        dp[-1] = True
-        for i in range(n - 2, -1, -1):
-            for step in range(nums[i]):
-                if dp[i + step + 1]:
-                    dp[i] = True
-                    break
-        return dp[0]
+        max_jump = 0
+        for i in range(n):
+            if i > max_jump:
+                return False
+            if max_jump >= n:
+                return True
+            max_jump = max(max_jump, i + nums[i])
+        return True
 
 
 print(Solution().canJump([3, 2, 1, 0, 4]))
