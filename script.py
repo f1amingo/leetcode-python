377771617fa64f54ba513b1644df2059b7587587
flamingo <zsjperiod@foxmail.com>
2019-11-23 14:35:53 +0800

    简化代码

--- script.py
+++ script.py
@@ -1,22 +1,2 @@
-from typing import List
-
-
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        ans = []
-
-        def dfs(cur, left, right):
-            if right == 0:
-                if cur:
-                    ans.append(cur)
-                return
-            if left > 0:
-                dfs(cur + '(', left - 1, right)
-            if right > left:
-                dfs(cur + ')', left, right - 1)
-
-        dfs('', n, n)
-        return ans
-
-
-print(Solution().generateParenthesis(0))
+intervals = [[1, 3], [2, 6], [15, 18], [8, 10], ]
+print(sorted(intervals))
