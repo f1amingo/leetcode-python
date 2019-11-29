89fc2003e6213f39245a23d195aade57565f861f
flamingo <zsjperiod@foxmail.com>
2019-11-29 20:31:07 +0800

    word break dp 1d

---
+++ hot-100/medium/139. Word Break.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        for i in range(0, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[-1]
+
+
+print(Solution().wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]))
