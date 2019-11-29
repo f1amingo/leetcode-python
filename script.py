cb61395f463e51194049094fb38400948c8be57f
flamingo <zsjperiod@foxmail.com>
2019-11-29 21:33:36 +0800

    Jump Game revice

--- script.py
+++ script.py
@@ -2,22 +2,13 @@ from typing import List
 
 
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        n = len(nums)
-        cut_point = -1
-        for i in range(n - 1, 0, -1):
-            if nums[i] > nums[i - 1]:
-                cut_point = i - 1
-                break
-        if cut_point != -1:
-            max_i = cut_point + 1
-            for i in range(cut_point + 1, n):
-                if nums[max_i] > nums[i] > nums[cut_point]:
-                    max_i = i
-            nums[cut_point], nums[max_i] = nums[max_i], nums[cut_point]
-        nums[cut_point + 1:] = sorted(nums[cut_point + 1:])
+    def canJump(self, nums: List[int]) -> bool:
+        left_most = 0
+        for i in range(len(nums)):
+            if left_most < i:
+                return False
+            left_most = max(left_most, i + nums[i])
+        return True
 
 
-arr = [1,1,5]
-Solution().nextPermutation(arr)
-print(arr)
+print(Solution().canJump([3, 2, 1, 0, 4]))
