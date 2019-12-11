9edf7f2c40d026824cbd4a32d34efdbc617cb130
flamingo <zsjperiod@foxmail.com>
2019-12-11 08:53:57 +0800

    Kth latgest number

---
+++ hot-100/medium/215. Kth Largest Element in an Array.py
@@ -0,0 +1,17 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # 排序方法
+        # nums.sort(reverse=True)
+        # return nums[k - 1]
+
+        # 使用堆
+        # return heapq.nlargest(k, nums)[-1]
+
+
+
+
+print(Solution().findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
