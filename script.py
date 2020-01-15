32a0113e5b412b6e3feb33b7c7be812193b8228f
flamingo <zsjperiod@foxmail.com>
2020-01-15 10:18:24 +0800

    merge intervals

--- script.py
+++ script.py
@@ -1,27 +1,26 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+from typing import List
 
 
 class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        if not head:
-            return head
-        slow = fast = head
-        while fast:
-            slow = slow.next
-            fast = fast.next
-            if not fast:
-                return None
-            fast = fast.next
-            if slow == fast:
-                break
-        if not fast:
-            return None
-        slow = head
-        while slow != fast:
-            slow = slow.next
-            fast = fast.next
-        return slow
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        sorted_list = sorted(intervals)
+        res = []
+        left = right = None
+        for inter in sorted_list:
+            if left is None and right is None:
+                left = inter[0]
+                right = inter[1]
+            else:
+                if right < inter[0]:
+                    res.append([left, right])
+                    left = inter[0]
+                    right = inter[1]
+                elif right < inter[1]:
+                    right = inter[1]
+        res.append([left, right])
+        return res
+
+
+print(Solution().merge([[1, 4], [2, 3]]))
