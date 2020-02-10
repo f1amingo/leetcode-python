0d62cc42c4f650221ed2e600c6c36c4e7121d36d
flamingo <zsjperiod@foxmail.com>
2020-02-10 11:33:34 +0800

    word break

--- script.py
+++ script.py
@@ -2,26 +2,18 @@ from typing import List
 
 
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return []
-        res = []
-        intervals.sort()
-        left = intervals[0][0]
-        right = intervals[0][1]
-        for item in intervals:
-            if item[0] <= right:
-                right = max(right, item[1])
-            elif item[0] <= left and item[1] <= right:
-                pass
-            else:
-                res.append([left, right])
-                left = item[0]
-                right = item[1]
-        res.append([left, right])
-        return res
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[-1]
 
 
-print(Solution().merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
-print(Solution().merge([[1, 4], [4, 5]]))
-print(Solution().merge([]))
+s = "leetcode"
+wordDict = ["leet", "code"]
+ans = Solution().wordBreak(s, wordDict)
