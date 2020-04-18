3ea0bee6a487b147fec337c774923d5b013c09a6
flamingo <zsjperiod@foxmail.com>
2020-04-18 10:30:12 +0800

    盛水最多的容器

---
+++ hard/面试题 16.03. 交点.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def intersection(self, start1: List[int], end1: List[int], start2: List[int], end2: List[int]) -> List[float]:
+
+
+
+start1 = [0, 0]
+end1 = [0, 1]
+start2 = [0, 2]
+end2 = [0, 3]
+print(Solution().intersection(start1, end1, start2, end2))
