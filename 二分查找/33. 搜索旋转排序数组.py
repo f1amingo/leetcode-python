8a8dde4f6d9067b86ad80f6cab76e2c69a8500b2
flamingo <zsjperiod@foxmail.com>
2020-04-27 11:48:01 +0800

    搜索旋转排序数组

---
+++ 二分查找/33. 搜索旋转排序数组.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        low, high = 0, len(nums) - 1
+        while low < high:
+            mid = low + (high - low) // 2
+            # [low, mid]有序
+            if nums[low] < nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid
+                else:
+                    low = mid + 1
+            # [mid+1, high]有序
+            else:
+                if nums[mid + 1] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid
+        return -1 if nums[low] != target else low
+
+
+nums = [4, 5, 6, 7, 0, 1, 2]
+target = 1
+print(Solution().search(nums, target))
