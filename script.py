d74b233ee0c94b2dd41bb295b824165a80c4e872
flamingo <zsjperiod@foxmail.com>
2020-05-04 11:44:22 +0800

    跳跃游戏

--- script.py
+++ script.py
@@ -1,4 +1,18 @@
-print(bin(-1))
-print(bin(1))
+from typing import List
 
-print(6 & (-6))
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxPos, n = 0, len(nums)
+        for i in range(n):
+            if maxPos < i:
+                return False
+            maxPos = max(maxPos, i + nums[i])
+            if maxPos >= n - 1:
+                return True
+        return True
+
+
+assert Solution().canJump([2, 3, 1, 1, 4])
+assert not Solution().canJump([3, 2, 1, 0, 4])
+assert Solution().canJump([])
