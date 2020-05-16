7bfdde1b141de873b590d46c06ae13dda853c878
flamingo <zsjperiod@foxmail.com>
2020-05-16 12:29:42 +0800

    反转链表 递归 迭代

--- 链表/25. K 个一组翻转链表.py
+++ 链表/25. K 个一组翻转链表.py
@@ -6,32 +6,37 @@ class ListNode:
 
 
 class Solution:
+    # my solution
+    # def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+    #     if not (head and head.next):
+    #         return head
+    #
+    #     self.n = 0
+    #     self.tail = None
+    #
+    #     def reverse(_head: ListNode, i: int):
+    #         self.n = i + 1
+    #         if not _head.next:
+    #             self.tail = _head
+    #             return _head
+    #         re_node = reverse(_head.next, i + 1)
+    #         if self.n - i <= self.n % k:
+    #             return _head
+    #         if (i + 1) % k == 0:
+    #             _head.next = re_node
+    #             self.tail = _head
+    #             return _head
+    #         else:
+    #             _head.next = self.tail.next
+    #             self.tail.next = _head
+    #             self.tail = _head
+    #             return re_node
+    #
+    #     return reverse(head, 0)
+
+    # 官方题解
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        if not (head and head.next):
-            return head
-
-        self.n = 0
-        self.tail = None
-
-        def reverse(_head: ListNode, i: int):
-            self.n = i + 1
-            if not _head.next:
-                self.tail = _head
-                return _head
-            re_node = reverse(_head.next, i + 1)
-            if self.n - i <= self.n % k:
-                return _head
-            if (i + 1) % k == 0:
-                _head.next = re_node
-                self.tail = _head
-                return _head
-            else:
-                _head.next = self.tail.next
-                self.tail.next = _head
-                self.tail = _head
-                return re_node
-
-        return reverse(head, 0)
+        pass
 
 
 n1 = ListNode(1)
