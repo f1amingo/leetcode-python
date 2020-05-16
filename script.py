2dd136914cf491947cbd1b21b601272c04ef53c2
flamingo <zsjperiod@foxmail.com>
2020-05-16 11:59:24 +0800

    25. K 个一组翻转链表

--- script.py
+++ script.py
@@ -1,18 +1,3 @@
-from typing import List
-
-
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        maxPos, n = 0, len(nums)
-        for i in range(n):
-            if maxPos < i:
-                return False
-            maxPos = max(maxPos, i + nums[i])
-            if maxPos >= n - 1:
-                return True
-        return True
-
-
-assert Solution().canJump([2, 3, 1, 1, 4])
-assert not Solution().canJump([3, 2, 1, 0, 4])
-assert Solution().canJump([])
+    def characterReplacement(self, s: str, k: int) -> int:
+        pass
---
+++ 链表/25. K 个一组翻转链表.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if not (head and head.next):
+            return head
+
+        self.n = 0
+        self.tail = None
+
+        def reverse(_head: ListNode, i: int):
+            self.n = i + 1
+            if not _head.next:
+                self.tail = _head
+                return _head
+            re_node = reverse(_head.next, i + 1)
+            if self.n - i <= self.n % k:
+                return _head
+            if (i + 1) % k == 0:
+                _head.next = re_node
+                self.tail = _head
+                return _head
+            else:
+                _head.next = self.tail.next
+                self.tail.next = _head
+                self.tail = _head
+                return re_node
+
+        return reverse(head, 0)
+
+
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n5 = ListNode(5)
+n1.next = n2
+n2.next = n3
+# n3.next = n4
+# n4.next = n5
+node = Solution().reverseKGroup(n1, 3)
+
+while node:
+    print(node.val)
+    node = node.next
