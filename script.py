69c5dbbf0f4fd32e26691c8f14f2f6eebb397b36
flamingoo <zsjperiod@foxmail.com>
2020-05-29 12:40:07 +0800

    统计优美子数组

--- script.py
+++ script.py
@@ -2,21 +2,27 @@ from typing import List
 
 
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        to_right, to_left = [0] * len(nums), [0] * len(nums)
-        cur_max = float('-inf')
-        for i in range(len(nums)):
-            cur_max = nums[i] if i % k == 0 else max(cur_max, nums[i])
-            to_right[i] = cur_max
-        cur_max = float('-inf')
-        for i in range(len(nums) - 1, -1, -1):
-            cur_max = nums[i] if (i + 1) % k == 0 else max(cur_max, nums[i])
-            to_left[i] = cur_max
-        ans = [0] * (len(nums) - k + 1)
-        for high in range(k - 1, len(nums)):
-            low = high - k + 1
-            ans[low] = max(to_right[high], to_left[low])
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        # 保存奇数下标
+        # 快速访问left-1和right+1
+        # 左边界初始为-1
+        odds = [-1]
+        for i, num in enumerate(nums):
+            if num % 2 == 1:
+                odds.append(i)
+        # 右边界初始为len(nums)
+        odds.append(len(nums))
+        left = right = 1
+        ans = 0
+        while right < len(odds) - 1:
+            if k == right - left + 1:
+                ans += (odds[left] - odds[left - 1]) * (odds[right + 1] - odds[right])
+                left += 1
+            right += 1
         return ans
 
 
-print(Solution().maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
+print(Solution().numberOfSubarrays([1, 1, 2, 1, 1], 3))
+print(Solution().numberOfSubarrays([2, 4, 6], 1))
+print(Solution().numberOfSubarrays([2, 2, 2, 1, 2, 2, 1, 2, 2, 2], 2))
+print(Solution().numberOfSubarrays([1], 1))
--- 双指针/1248. 统计「优美子数组」.py
+++ 双指针/1248. 统计「优美子数组」.py
@@ -2,19 +2,40 @@ from typing import List
 
 
 class Solution:
+    # 之前
+    # def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+    #     n = len(nums)
+    #     odd_list = []
+    #     for i in range(n):
+    #         if nums[i] & 1 == 1:
+    #             odd_list.append(i)
+    #     odd_n = len(odd_list)
+    #     ans = 0
+    #     for right in range(k - 1, odd_n):
+    #         left = right - k + 1
+    #         left_count = odd_list[left] + 1 if left == 0 else odd_list[left] - odd_list[left - 1]
+    #         right_count = n - odd_list[right] if right == odd_n - 1 else odd_list[right + 1] - odd_list[right]
+    #         ans += left_count * right_count
+    #     return ans
+
+    # 重做
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        odd_list = []
-        for i in range(n):
-            if nums[i] & 1 == 1:
-                odd_list.append(i)
-        odd_n = len(odd_list)
+        # 保存奇数下标
+        # 快速访问left-1和right+1
+        # 左边界初始为-1
+        odds = [-1]
+        for i, num in enumerate(nums):
+            if num % 2 == 1:
+                odds.append(i)
+        # 右边界初始为len(nums)
+        odds.append(len(nums))
+        left = right = 1
         ans = 0
-        for right in range(k - 1, odd_n):
-            left = right - k + 1
-            left_count = odd_list[left] + 1 if left == 0 else odd_list[left] - odd_list[left - 1]
-            right_count = n - odd_list[right] if right == odd_n - 1 else odd_list[right + 1] - odd_list[right]
-            ans += left_count * right_count
+        while right < len(odds) - 1:
+            if k == right - left + 1:
+                ans += (odds[left] - odds[left - 1]) * (odds[right + 1] - odds[right])
+                left += 1
+            right += 1
         return ans
 
 
