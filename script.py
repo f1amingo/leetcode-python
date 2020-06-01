60076bf456eeee82ff75296f9996de66dd5f1c15
flamingo <zsjperiod@foxmail.com>
2020-06-01 10:26:55 +0800

    合并K个排序链表

--- script.py
+++ script.py
@@ -1,44 +1,61 @@
+# Definition for singly-linked list.
 from typing import List
 
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution:
-    def reversePairs(self, nums: List[int]) -> int:
-        ans = 0
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return None
 
-        def merge(low, mid, high):
-            i, j, k = low, mid, 0
-            arr = [0] * (high - low)
-            while i < mid and j < high:
-                if nums[i] <= nums[j]:
-                    arr[k] = nums[i]
-                    i += 1
+        def merge(low, high):
+            list1, list2 = lists[low], lists[high]
+            dummy = ListNode(-1)
+            ptr1, ptr2, ptr3 = list1, list2, dummy
+            while ptr1 and ptr2:
+                if ptr1.val <= ptr2.val:
+                    ptr3.next = ptr1
+                    ptr1 = ptr1.next
                 else:
-                    arr[k] = nums[j]
-                    j += 1
-                    nonlocal ans
-                    ans += mid - i
-                k += 1
-            while i < mid:
-                arr[k] = nums[i]
-                k += 1
-                i += 1
-            while j < high:
-                arr[k] = nums[j]
-                k += 1
-                j += 1
-            nums[low:high] = arr
+                    ptr3.next = ptr2
+                    ptr2 = ptr2.next
+                ptr3 = ptr3.next
+            ptr3.next = ptr1 if ptr1 else ptr2
+            lists[low] = dummy.next
 
-        def sort(low, high):
-            if low < high - 1:
+        def partition(low, high):
+            if low < high:
                 mid = (low + high) // 2
-                sort(low, mid)
-                sort(mid, high)
-                if nums[mid - 1] <= nums[mid]:
-                    return
-                merge(low, mid, high)
+                partition(low, mid)
+                partition(mid + 1, high)
+                merge(low, mid + 1)
 
-        sort(0, len(nums))
-        return ans
+        partition(0, len(lists) - 1)
+
+        return lists[0]
 
 
-print(Solution().reversePairs([7, 5, 6, 4]))
+a1 = ListNode(1)
+a2 = ListNode(2)
+a3 = ListNode(3)
+a1.next = a2
+a2.next = a3
+b1 = ListNode(4)
+b2 = ListNode(5)
+b3 = ListNode(6)
+b4 = ListNode(7)
+b1.next = b2
+b2.next = b3
+b3.next = b4
+
+node = Solution().mergeKLists([a1, b1])
+pri = []
+while node:
+    pri.append(node.val)
+    node = node.next
+print(pri)
