b97c89fbd3f13b299e42b42c4763e125c382568f
flamingo <zsjperiod@foxmail.com>
2020-06-26 18:28:45 +0800

    解码方法

--- script.py
+++ script.py
@@ -1,20 +1,2 @@
-from typing import List
-
-
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        if len(nums) <= 1:
-            return 0
-        maxPos, step, end = 0, 0, 0
-        for i in range(len(nums)):
-            maxPos = max(maxPos, i + nums[i])
-            if maxPos >= len(nums) - 1:
-                return step + 1
-            if i == end:
-                end = maxPos
-                step += 1
-        return step
-
-
-print(Solution().jump([2, 3, 1, 1, 4]))
-print(Solution().jump([1]))
+s = '123'
+print(int(s[1:2]))
