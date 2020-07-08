22263f38c7021a5a61448074a586ba617f5887d9
flamingo <zsjperiod@foxmail.com>
2020-07-08 16:37:36 +0800

    416. 分割等和子集

---
+++ 动态规划/416. 分割等和子集.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+
+    def canPartition(self, nums: List[int]) -> bool:
+        # 朴素01背包
+        # total = sum(nums)
+        # if total % 2 == 1:
+        #     return False
+        # C = total // 2
+        # dp = [0] * (C + 1)
+        # for num in nums:
+        #     for j in range(C, num - 1, -1):
+        #         dp[j] = max(dp[j], dp[j - num] + num)
+        # return dp[-1] == C
+
+        # bool运算更快
+        # dp[i]：前k个数字是否可以凑出C
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        C = total // 2
+        dp = [False] * (C + 1)
+        dp[0] = True
+        for num in nums:
+            for j in range(C, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+        return dp[-1]
+
+
+# print(Solution().canPartition([1, 5, 11, 5]))
+print(Solution().canPartition([1, 2, 5]))
