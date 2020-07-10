d71e6b4d9bcaad24cbd8900a2af328f3547d541a
flamingo <zsjperiod@foxmail.com>
2020-07-10 14:03:49 +0800

    523. 连续的子数组和

---
+++ 动态规划/523. 连续的子数组和.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        if n <= 1:
+            return False
+        pre = [0] * (n + 1)
+        for i in range(n):
+            pre[i + 1] = pre[i] + nums[i]
+        for i in range(1, n):
+            for j in range(i + 1, n + 1):
+                p_sum = pre[j] - pre[i - 1]
+                # 当使用%或者/时，小心0
+                if k == 0:
+                    if p_sum == 0:
+                        return True
+                elif p_sum % k == 0:
+                    return True
+        return False
+
+
+print(Solution().checkSubarraySum([7, 6], 6))
