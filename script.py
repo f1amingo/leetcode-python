008fd173a01b2d86794e3093675c44fb350a8123
flamingo <zsjperiod@foxmail.com>
2020-07-14 20:59:48 +0800

    413. 等差数列划分

--- script.py
+++ script.py
@@ -2,21 +2,17 @@ from typing import List
 
 
 class Solution:
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        if not nums:
-            return False
-        n = len(nums)
-        pre = [0] * (n + 1)
-        pre[1] = nums[0]
-        for i in range(1, n):
-            pre[i + 1] = pre[i] + nums[i]
-            for j in range(i):
-                if k == 0:
-                    if pre[i + 1] - pre[j] == 0:
-                        return True
-                elif (pre[i + 1] - pre[j]) % k == 0:
-                    return True
-        return False
+    def numberOfArithmeticSlices(self, A: List[int]) -> int:
+        n = len(A)
+        if n < 3:
+            return 0
+        dp = [0] * n
+        res = 0
+        for i in range(2, n):
+            if A[i] + A[i - 2] == 2 * A[i - 1]:
+                dp[i] = dp[i - 1] + 1
+                res += dp[i]
+        return res
 
 
-print(Solution().checkSubarraySum([0, 0], 0))
+print(Solution().numberOfArithmeticSlices([1, 2, 3, 6, 9]))
