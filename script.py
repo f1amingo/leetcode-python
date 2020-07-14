5dc97387400841ec0120a87b7ce257826911b32c
flamingo <zsjperiod@foxmail.com>
2020-07-14 15:11:42 +0800

    33. 搜索旋转排序数组

--- script.py
+++ script.py
@@ -2,19 +2,23 @@ from typing import List
 
 
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        n = len(nums)
-        if n <= 1:
-            return False
-        total = sum(nums)
-        if total % 2 == 1:
-            return False
-        C = total // 2
-        dp = [0] * (C + 1)
-        for num in nums:
-            for v in range(C, num - 1, -1):
-                dp[v] = max(dp[v], dp[v - num] + num)
-        return dp[-1] == C
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid
+        return left if nums[left] == target else -1
 
 
-print(Solution().canPartition([23, 13, 11, 7, 6, 5, 5]))
+print(Solution().search([4, 5, 6, 7, 0, 1, 2], 3))
