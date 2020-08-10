cb21a5b7abe009bdc3eb6a88c9ea75f489594539
Flamingoo <zsjperiod@foxmail.com>
2020-08-10 14:42:25 +0800

    剑指 Offer 44. 数字序列中某一位的数字

---
+++ 剑指offer/剑指 Offer 44. 数字序列中某一位的数字.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findNthDigit(self, n: int) -> int:
+        start, digit, count = 1, 1, 9
+        while n > count:
+            n -= count
+            start *= 10
+            digit += 1
+            count = 9 * start * digit
+        num = start + (n - 1) // digit
+        return int(str(num)[(n - 1) % digit])
+
+
+print(
+    Solution().findNthDigit(3)
+)
