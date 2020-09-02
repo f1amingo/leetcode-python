2de13eb56d336de86638173790a0367e88854a78
flamingo <zsjperiod@foxmail.com>
2020-09-02 12:53:22 +0800

    378. 有序矩阵中第K小的元素

---
+++ 二分查找/378. 有序矩阵中第K小的元素.py
@@ -0,0 +1,26 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    # 使用heap
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        if not matrix or not matrix[0]:
+            return -1
+        # 第k小，就是第N-k+1大
+        n_largest = len(matrix) ** 2 - k + 1
+        heap = []
+        for row in matrix:
+            for ele in row:
+                heapq.heappush(heap, ele)
+                if len(heap) > n_largest:
+                    heapq.heappop(heap)
+        return heap[0]
+
+
+matrix = [
+    [1, 5, 9],
+    [10, 11, 13],
+    [12, 13, 15]
+]
+print(Solution().kthSmallest(matrix, 8))
