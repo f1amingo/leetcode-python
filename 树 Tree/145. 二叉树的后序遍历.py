23d06922c9d1b4c630f6f4285f98e9e2aaaddecc
flamingo <zsjperiod@foxmail.com>
2020-09-13 13:41:31 +0800

    145. 二叉树的后序遍历

---
+++ 树 Tree/145. 二叉树的后序遍历.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from util.ZTree import *
+
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res, stk = [], [root]
+        while stk:
+            node = stk.pop()
+            if node.left:
+                stk.append(node.left)
+            if node.right:
+                stk.append(node.right)
+            res.insert(0, node.val)  # 头插
+        return res
+
+    # recursion
+    # def postorderTraversal(self, root: TreeNode) -> List[int]:
+    #     if not root:
+    #         return []
+    #
+    #     def dfs(r: TreeNode):
+    #         if r:
+    #             nonlocal res
+    #             dfs(r.left)
+    #             dfs(r.right)
+    #             res.append(r.val)
+    #
+    #     res = []
+    #     dfs(root)
+    #     return res
+
+
+print(Solution().postorderTraversal())
