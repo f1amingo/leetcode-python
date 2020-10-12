e66e5af11f0347ad306f4d9fe5256d2fade95725
flamingo <zsjperiod@foxmail.com>
2020-10-12 13:06:13 +0800

    剑指 Offer 04. 二维数组中的查找

---
+++ 剑指offer/剑指 Offer 04. 二维数组中的查找.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+# 时间复杂度 O(m+n)
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        # i, j = m, 0 第一次写错了，低级错误
+        i, j = m - 1, 0
+        while i > -1 and j < n:
+            if target < matrix[i][j]:
+                i -= 1
+            elif target > matrix[i][j]:
+                j += 1
+            else:
+                return True
+        return False
