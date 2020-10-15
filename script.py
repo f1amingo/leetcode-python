833fab330cd6ec27bf9c22caf8b3b05ce45f9b09
flamingo <zsjperiod@foxmail.com>
2020-10-15 14:24:10 +0800

    review 剑指 Offer 16. 数值的整数次方

--- script.py
+++ script.py
@@ -1,18 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def dfs(k: int) -> float:
-            if k == 0:
-                return 1
-            div, mod = divmod(k, 2)
-            _res = dfs(div)
-            _res *= _res
-            if mod == 1:
-                _res *= x
-            return _res
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        res = 1
+        while n:
+            if n & 1:
+                res *= x
+            x *= x
+            n >>= 1
+        return res
 
-        return dfs(n) if n > 0 else 1 / dfs(-n)
 
-
-print(Solution().myPow(2, 10))
-print(Solution().myPow(2.1, 3))
 print(Solution().myPow(2, -2))
+print(Solution().myPow(2, 10))
