269f29e63f5077502e6e5f5e3c637707d8d5f89e
flamingo <zsjperiod@foxmail.com>
2020-11-24 20:58:55 +0800

    1143. 最长公共子序列

--- script.py
+++ script.py
@@ -1,6 +1,15 @@
 class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        m, n = len(s), len(t)
-        if m > n:
-            return False
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[-1][-1]
 
+
+assert Solution().longestCommonSubsequence('abc', 'abc') == 3
+assert Solution().longestCommonSubsequence('abc', 'def') == 0
