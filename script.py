bb29973b477361ed7f2ea5c8339a5059f22ab6ab
flamingo <zsjperiod@foxmail.com>
2020-11-29 21:40:24 +0800

    516. 最长回文子序列

--- script.py
+++ script.py
@@ -1,44 +1,20 @@
 class Solution:
-    def numDecodings(self, s: str) -> int:
-        if not s:
-            return 0
-        # 前导零不合法
-        if s[0] == '0':
-            return 0
+    def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
-        pre, cur = 1, 1
-        for i in range(1, n):
-            num = int(s[i - 1:i + 1])
-            # s[i]为零，只能联合解码，但是s[i-1]s[i]又不合法
-            if s[i] == '0' and num > 26:
-                return 0
-            if num == 0:
-                # 连续两个0
-                return 0
-            elif num < 10 or num > 26:
-                # num < 10: 01到09，不能有前导零，s[i]独自解码
-                # num > 26: 不再有意义，s[i]独自解码
-                tmp = cur
-            elif num == 10 or num == 20:
-                # s[i]为0，s[i-1]s[i]联合解码
-                tmp = pre
-            elif num <= 26:
-                # s[i]可以独自解码
-                # s[i-1]s[i]可以联合解码
-                tmp = cur + pre
-            pre, cur = cur, tmp
-        return cur
+        if n < 2:
+            return n
+        dp = [[1] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    if j - i == 1:
+                        dp[i][j] = 2
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i + 1][j])
+        return dp[0][-1]
 
 
-assert Solution().numDecodings("301") == 0  # 中间不合法的0
-assert Solution().numDecodings("230") == 0  # 单独一个0无法解码
-assert Solution().numDecodings("1201234") == 3
-assert Solution().numDecodings("01") == 0  # 前导零不合法
-assert Solution().numDecodings("10011") == 0  # 连续两个0无效
-assert Solution().numDecodings("2101") == 1  # 1,2,...,9,10只有一种解法
-assert Solution().numDecodings('10') == 1
-assert Solution().numDecodings('12') == 2
-assert Solution().numDecodings('226') == 3
-assert Solution().numDecodings('0') == 0
-assert Solution().numDecodings('1') == 1
-assert Solution().numDecodings('2') == 1
+assert Solution().longestPalindromeSubseq('bbbab') == 4
+assert Solution().longestPalindromeSubseq('cbbd') == 2
