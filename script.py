b72873959a8f4fdc74604db831876fd27c6ed330
flamingo <zsjperiod@foxmail.com>
2020-12-17 14:47:57 +0800

    337. 打家劫舍 III

--- script.py
+++ script.py
@@ -1,47 +1,24 @@
-from typing import List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from util.ZTree import TreeNode
 
 
 class Solution:
-    # 奇数比前一个数多一个1
-    # 偶数与右移后相同
-    def countBits(self, num: int) -> List[int]:
-        dp = [0] * (num + 1)
-        for i in range(1, num + 1):
-            # 奇数
-            if i & 1 == 1:
-                dp[i] = dp[i - 1] + 1
-            else:
-                dp[i] = dp[i >> 1]
-        return dp
+    # 每个节点返回(偷，不偷)
+    def rob(self, root: TreeNode) -> int:
+        def dfs(r: TreeNode) -> (int, int):
+            if not r:
+                return 0, 0
+            left, right = dfs(r.left), dfs(r.right)
+            do_rob = r.val + left[1] + right[1]
+            no_rob = max(left) + max(right)
+            return do_rob, no_rob
 
-    # 自己琢磨出来的while True...
-    # 按4,8,16分段计算
-    # def countBits(self, num: int) -> List[int]:
-    #     if num < 4:
-    #         return [0, 1, 1, 2][:num + 1]
-    #     ans = [0, 1, 1, 2] + [0] * (num + 1 - 4)
-    #     offset = 4
-    #     while True:
-    #         for i in range(offset, 2 * offset):
-    #             ans[i] = ans[i - offset] + 1
-    #             if i == num:
-    #                 return ans
-    #         offset *= 2
-
-    # O(n*sizeof(inte   ger))
-    # 模拟，有点慢
-    # def countBits(self, num: int) -> List[int]:
-    #     ans = [0] * (num + 1)
-    #     for i in range(num + 1):
-    #         cur, count = i, 0
-    #         while cur != 0:
-    #             # 最低位
-    #             count += 1 if cur & 1 == 1 else 0
-    #             # 右移更新最低位
-    #             cur >>= 1
-    #         ans[i] = count
-    #     return ans
+        return max(dfs(root))
 
 
-assert Solution().countBits(2) == [0, 1, 1]
-assert Solution().countBits(5) == [0, 1, 1, 2, 1, 2]
+Solution().rob()
