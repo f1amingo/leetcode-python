caa3003be2af1d960bf9ee4dd701754404075815
flamingo <zsjperiod@foxmail.com>
2020-12-18 10:54:13 +0800

    238. 除自身以外数组的乘积

--- script.py
+++ script.py
@@ -2,22 +2,21 @@ from typing import List
 
 
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix or not matrix[0]:
-            return False
-        m, n = len(matrix), len(matrix[0])
-        i, j = 0, n - 1
-        while i < m and j > -1:
-            if target == matrix[i][j]:
-                return True
-            elif target < matrix[i][j]:
-                j -= 1
-            else:
-                i += 1
-        return False
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pre, post = [0] * n, [0] * n
+        tmp = 1
+        for i in range(n):
+            tmp *= nums[i]
+            pre[i] = tmp
+        tmp = 1
+        for i in range(n - 1, -1, -1):
+            tmp *= nums[i]
+            post[i] = tmp
+        ans = [0] * n
+        for i in range(n):
+            ans[i] = (pre[i - 1] if i != 0 else 1) * (post[i + 1] if i != n - 1 else 1)
+        return ans
 
 
-assert Solution().searchMatrix(
-    [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 5)
-assert not Solution().searchMatrix(
-    [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20)
+assert Solution().productExceptSelf([1, 2, 3, 4]) == [24, 12, 8, 6]
