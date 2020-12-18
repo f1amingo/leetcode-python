eeb0d7255ce483e389e9f9beb1da48612d9a6198
flamingo <zsjperiod@foxmail.com>
2020-12-18 15:36:44 +0800

    236. 二叉树的最近公共祖先

--- script.py
+++ script.py
@@ -1,22 +1,30 @@
-from typing import List
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        pre, post = [0] * n, [0] * n
-        tmp = 1
-        for i in range(n):
-            tmp *= nums[i]
-            pre[i] = tmp
-        tmp = 1
-        for i in range(n - 1, -1, -1):
-            tmp *= nums[i]
-            post[i] = tmp
-        ans = [0] * n
-        for i in range(n):
-            ans[i] = (pre[i - 1] if i != 0 else 1) * (post[i + 1] if i != n - 1 else 1)
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def findPQ(r: 'TreeNode') -> (bool, bool):
+            if not r:
+                return False, False
+            hasP, hasQ = (r.val == p.val), (r.val == q.val)
+            ltP, ltQ = findPQ(r.left)
+            hasP, hasQ = hasP or ltP, hasQ or ltQ
+            # 左子树和当前节点已经含有P, Q
+            # 则没有必要再递归右子树
+            if not hasP or not hasQ:
+                rtP, rtQ = findPQ(r.right)
+                hasP, hasQ = hasP or rtP, hasQ or rtQ
+            # 第一次同时找到P, Q，更新结果
+            if hasP and hasQ:
+                nonlocal ans
+                if not ans:
+                    ans = r
+            return hasP, hasQ
+
+        ans = None
+        findPQ(root)
         return ans
-
-
-assert Solution().productExceptSelf([1, 2, 3, 4]) == [24, 12, 8, 6]
--- 树 Tree/236. 二叉树的最近公共祖先.py
+++ 树 Tree/236. 二叉树的最近公共祖先.py
@@ -8,6 +8,7 @@ from util.ZTree import *
 
 
 class Solution:
+    # 别人的
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root or root == p or root == q:
             return root
@@ -22,6 +23,31 @@ class Solution:
             return left
         # left, right都不为空，则p, q分布在左右两侧，返回当前节点
         return root
+
+    # 自己琢磨出来的解法
+    # def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    #     # P, Q是否存在当前树下
+    #     def findPQ(r: 'TreeNode') -> (bool, bool):
+    #         if not r:
+    #             return False, False
+    #         hasP, hasQ = (r.val == p.val), (r.val == q.val)
+    #         ltP, ltQ = findPQ(r.left)
+    #         hasP, hasQ = hasP or ltP, hasQ or ltQ
+    #         # 左子树和当前节点已经含有P, Q
+    #         # 则没有必要再递归右子树
+    #         if not hasP or not hasQ:
+    #             rtP, rtQ = findPQ(r.right)
+    #             hasP, hasQ = hasP or rtP, hasQ or rtQ
+    #         # 第一次同时找到P, Q，更新结果
+    #         if hasP and hasQ:
+    #             nonlocal ans
+    #             if not ans:
+    #                 ans = r
+    #         return hasP, hasQ
+    #
+    #     ans = None
+    #     findPQ(root)
+    #     return ans
 
 
 roo = buildTreeFromList([3, 5, 1, 6, 2, 10, 8, None, None, 7, 4])
