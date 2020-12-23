05acf804b2ea869438dc8bd093b28bcab389e5ed
flamingo <zsjperiod@foxmail.com>
2020-12-23 19:21:26 +0800

    49. 字母异位词分组

--- script.py
+++ script.py
@@ -1,15 +1,17 @@
+import collections
 from typing import List
 
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        right_most = 0
-        for i, num in enumerate(nums):
-            if right_most < i:
-                return False
-            right_most = max(right_most, i + num)
-        return True
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        n = len(strs)
+        sorted_strs = [''] * n
+        for i in range(n):
+            sorted_strs[i] = ''.join(sorted(strs[i]))
+        ans_dict = collections.defaultdict(list)
+        for i in range(n):
+            ans_dict[sorted_strs[i]].append(strs[i])
+        return list(ans_dict.values())
 
 
-assert Solution().canJump([2, 3, 1, 1, 4])
-assert not Solution().canJump([3, 2, 1, 0, 4])
+print(Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
