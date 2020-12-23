c2318fa0016054b46c7eadb1bf918917bfa8edca
flamingo <zsjperiod@foxmail.com>
2020-12-23 17:58:13 +0800

    64. 最小路径和

--- script.py
+++ script.py
@@ -2,18 +2,22 @@ from typing import List
 
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        def dfs(start: int, cur_set: []):
-            ans.append(cur_set.copy())
-            for i in range(start, n):
-                cur_set.append(nums[i])
-                dfs(i + 1, cur_set)
-                cur_set.pop()
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                elif i == 0:
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
+        return dp[-1][-1]
 
-        n = len(nums)
-        ans = []
-        dfs(0, [])
-        return ans
 
-
-print(Solution().subsets([1, 2, 3]))
+assert Solution().minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]) == 7
