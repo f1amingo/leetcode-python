2da268efbc8a0ecf98764139bf139e604e9deeac
flamingo <zsjperiod@foxmail.com>
2020-12-23 16:30:32 +0800

    78. 子集

--- script.py
+++ script.py
@@ -2,44 +2,18 @@ from typing import List
 
 
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(x: int, y: int, i: int) -> bool:
-            if i == len(word):
-                return True
-            if x < 0 or y < 0 or x >= m or y >= n or visited[x][y]:
-                return False
-            if board[x][y] == word[i]:
-                visited[x][y] = True
-                for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                    if dfs(x + dx, y + dy, i + 1):
-                        visited[x][y] = False
-                        return True
-                visited[x][y] = False
-            return False
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def dfs(start: int, cur_set: []):
+            ans.append(cur_set.copy())
+            for i in range(start, n):
+                cur_set.append(nums[i])
+                dfs(i + 1, cur_set)
+                cur_set.pop()
 
-        if not board or not board[0]:
-            return False
-        m, n = len(board), len(board[0])
-        visited = [[False] * n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                if dfs(i, j, 0):
-                    return True
-        return False
+        n = len(nums)
+        ans = []
+        dfs(0, [])
+        return ans
 
 
-assert Solution().exist([
-    ['A', 'B', 'C', 'E'],
-    ['S', 'F', 'C', 'S'],
-    ['A', 'D', 'E', 'E']
-], 'ABCCED')
-assert Solution().exist([
-    ['A', 'B', 'C', 'E'],
-    ['S', 'F', 'C', 'S'],
-    ['A', 'D', 'E', 'E']
-], 'SEE')
-assert not Solution().exist([
-    ['A', 'B', 'C', 'E'],
-    ['S', 'F', 'C', 'S'],
-    ['A', 'D', 'E', 'E']
-], 'ABCB')
+print(Solution().subsets([1, 2, 3]))
