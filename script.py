d3493156205abb57811a1ae342927c47dfacc374
flamingo <zsjperiod@foxmail.com>
2020-12-23 18:06:51 +0800

    62. 不同路径

--- script.py
+++ script.py
@@ -1,23 +1,19 @@
-from typing import List
-
-
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-        m, n = len(grid), len(grid[0])
+    def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 if i == 0 and j == 0:
-                    dp[i][j] = grid[i][j]
+                    dp[i][j] = 1
                 elif i == 0:
-                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                    dp[i][j] = dp[i][j - 1]
                 elif j == 0:
-                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                    dp[i][j] = dp[i - 1][j]
                 else:
-                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         return dp[-1][-1]
 
 
-assert Solution().minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]) == 7
+assert Solution().uniquePaths(3, 2) == 3
+assert Solution().uniquePaths(7, 3) == 28
+assert Solution().uniquePaths(3, 3) == 6
