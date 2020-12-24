23e34ac769ea8909d381df39402ff5dff0df4f56
flamingo <zsjperiod@foxmail.com>
2020-12-24 15:57:38 +0800

    39. 组合总和

--- script.py
+++ script.py
@@ -1,21 +1,26 @@
 from typing import List
 
 
+# 无重复元素，正整数
+# 可重复选择
+# 所有组合
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(start: int):
-            if start == n:
-                ans.append(nums.copy())
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(start: int, t: int, cur_set: List):
+            if t <= 0:
+                if t == 0:
+                    ans.append(cur_set.copy())
                 return
             for i in range(start, n):
-                nums[i], nums[start] = nums[start], nums[i]
-                dfs(start + 1)
-                nums[i], nums[start] = nums[start], nums[i]
+                cur_set.append(candidates[i])
+                dfs(i, t - candidates[i], cur_set)
+                cur_set.pop()
 
-        n = len(nums)
+        n = len(candidates)
         ans = []
-        dfs(0)
+        dfs(0, target, [])
         return ans
 
 
-print(Solution().permute([1, 2, 3]))
+print(Solution().combinationSum([2, 3, 5], 8))
+print(Solution().combinationSum([2, 3, 6, 7], 7))
--- 回溯算法 Backtracking/39. 组合总和.py
+++ 回溯算法 Backtracking/39. 组合总和.py
@@ -1,21 +1,41 @@
 from typing import List
 
 
+# 无重复元素，正整数
+# 可重复选择
+# 所有组合
 class Solution:
+    # 更加优美的代码
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(cur_set: List, cur_sum: int, res: List, start: int):
-            for i in range(start, len(candidates)):
-                new_sum = cur_sum + candidates[i]
-                if new_sum == target:
-                    res.append(cur_set + [candidates[i]])
-                elif new_sum < target:
-                    cur_set.append(candidates[i])
-                    dfs(cur_set, new_sum, res, i)
-                    cur_set.pop()
+        def dfs(start: int, t: int, cur_set: List):
+            if t <= 0:
+                if t == 0:
+                    ans.append(cur_set.copy())
+                return
+            for i in range(start, n):
+                cur_set.append(candidates[i])
+                dfs(i, t - candidates[i], cur_set)
+                cur_set.pop()
 
-        res = []
-        dfs([], 0, res, 0)
-        return res
+        n = len(candidates)
+        ans = []
+        dfs(0, target, [])
+        return ans
+
+    # def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    #     def dfs(cur_set: List, cur_sum: int, res: List, start: int):
+    #         for i in range(start, len(candidates)):
+    #             new_sum = cur_sum + candidates[i]
+    #             if new_sum == target:
+    #                 res.append(cur_set + [candidates[i]])
+    #             elif new_sum < target:
+    #                 cur_set.append(candidates[i])
+    #                 dfs(cur_set, new_sum, res, i)
+    #                 cur_set.pop()
+    #
+    #     res = []
+    #     dfs([], 0, res, 0)
+    #     return res
 
 
 print(Solution().combinationSum([5], 6))
