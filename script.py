cd3ca583f1f5be022261d459a3d54056a6d02717
flamingo <zsjperiod@foxmail.com>
2020-12-25 13:32:56 +0800

    11. 盛最多水的容器

--- script.py
+++ script.py
@@ -2,29 +2,21 @@ from typing import List
 
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        n = len(nums)
-        ans = []
-        for a in range(n - 2):
-            if nums[a] > 0:
-                break
-            if a > 0 and nums[a] == nums[a - 1]:
-                continue
-            b, c = a + 1, n - 1
-            while b < c:
-                tmp = nums[a] + nums[b] + nums[c]
-                if tmp == 0:
-                    ans.append([nums[a], nums[b], nums[c]])
-                if tmp >= 0:
-                    c -= 1
-                    while b < c and nums[c] == nums[c + 1]:
-                        c -= 1
-                if tmp <= 0:
-                    b += 1
-                    while b < c and nums[b] == nums[b - 1]:
-                        b += 1
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        lo, hi = 0, n - 1
+        ans = 0
+        while lo < hi:
+            if height[lo] < height[hi]:
+                ans = max(ans, (hi - lo) * height[lo])
+                lo += 1
+            else:
+                ans = max(ans, (hi - lo) * height[hi])
+                hi -= 1
         return ans
 
 
-print(Solution().threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]))
+assert Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
+assert Solution().maxArea([1, 1]) == 1
+assert Solution().maxArea([4, 3, 2, 1, 4]) == 16
+assert Solution().maxArea([1, 2, 1]) == 2
--- 数组 Array/11. 盛最多水的容器.py
+++ 数组 Array/11. 盛最多水的容器.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    # 相等的时候，移谁都可以，之后的更大值，只有在两块板都移出去之后才会出现
     def maxArea(self, height: List[int]) -> int:
         n = len(height)
         left = 0
