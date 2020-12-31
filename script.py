7666ee66d09eadb461130bacd94cef0b44240c2c
flamingo <zsjperiod@foxmail.com>
2020-12-31 10:27:45 +0800

    516. 最长回文子序列

--- script.py
+++ script.py
@@ -1,40 +1,21 @@
-# 回文子串，连续
-# dp[i][j]：是否是回文子串
-# dp[i][j] -> dp[i+1][j-1]
 class Solution:
-    # 中心扩散
-    def longestPalindrome(self, s: str) -> str:
-        def dfs(i: int, j: int):
-            while i >= 0 and j < n and s[i] == s[j]:
-                i -= 1
-                j += 1
-            i, j = i + 1, j - 1
-            nonlocal lt, rt
-            if j - i > rt - lt:
-                lt, rt = i, j
-
+    # f[i][j]: [i, j]中最长回文子串的长度
+    def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
-        lt, rt = 0, 0
-        for i in range(n - 1):
-            dfs(i, i)
-            dfs(i, i + 1)
-        return s[lt:rt + 1]
-
-    # def longestPalindrome(self, s: str) -> str:
-    #     n = len(s)
-    #     lt, rt = 0, 0
-    #     dp = [[False] * n for _ in range(n)]
-    #     for i in range(n - 1, -1, -1):
-    #         dp[i][i] = True
-    #         for j in range(i + 1, n):
-    #             if s[i] == s[j]:
-    #                 dp[i][j] = True if j - i <= 1 else dp[i + 1][j - 1]
-    #             if dp[i][j] and j - i > rt - lt:
-    #                 lt, rt = i, j
-    #     return s[lt:rt + 1]
+        if n <= 1:
+            return n
+        f = [[0] * n for _ in range(n)]
+        ans = 0
+        for i in range(n - 1, -1, -1):
+            f[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    f[i][j] = f[i + 1][j - 1] + 2
+                else:
+                    f[i][j] = max(f[i + 1][j], f[i][j - 1])
+                ans = max(ans, f[i][j])
+        return ans
 
 
-assert Solution().longestPalindrome('babad') == 'bab'
-assert Solution().longestPalindrome('cbbd') == 'bb'
-assert Solution().longestPalindrome('a') == 'a'
-assert Solution().longestPalindrome('') == ''
+assert Solution().longestPalindromeSubseq('bbbab') == 4
+assert Solution().longestPalindromeSubseq('a') == 1
