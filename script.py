fdb8ed6dc8c7ef19f53a3ff6f3cf2b0810ea2666
flamingo <zsjperiod@foxmail.com>
2020-12-31 10:44:30 +0800

    494. 目标和

--- script.py
+++ script.py
@@ -1,21 +1,19 @@
+from typing import List
+
+
+# 非负整数数组
 class Solution:
-    # f[i][j]: [i, j]中最长回文子串的长度
-    def longestPalindromeSubseq(self, s: str) -> int:
-        n = len(s)
-        if n <= 1:
-            return n
-        f = [[0] * n for _ in range(n)]
-        ans = 0
-        for i in range(n - 1, -1, -1):
-            f[i][i] = 1
-            for j in range(i + 1, n):
-                if s[i] == s[j]:
-                    f[i][j] = f[i + 1][j - 1] + 2
-                else:
-                    f[i][j] = max(f[i + 1][j], f[i][j - 1])
-                ans = max(ans, f[i][j])
-        return ans
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        C, mod = divmod(sum(nums) - S, 2)
+        if C < 0 or mod == 1:
+            return 0
+        dp = [1] + [0] * C
+        for num in nums:
+            for i in range(C, num - 1, -1):
+                dp[i] = dp[i] + dp[i - num]
+        return dp[-1]
 
 
-assert Solution().longestPalindromeSubseq('bbbab') == 4
-assert Solution().longestPalindromeSubseq('a') == 1
+assert Solution().findTargetSumWays([1, 2, 7, 9, 981], 1000000000) == 1
+assert Solution().findTargetSumWays([1000], -1000) == 1
+assert Solution().findTargetSumWays([1, 1, 1, 1, 1], 3) == 5
