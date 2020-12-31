4fba4a6581d6591acc0231bba5a9d46f98c6d5fa
flamingo <zsjperiod@foxmail.com>
2020-12-31 09:42:29 +0800

    236. 二叉树的最近公共祖先 review

--- script.py
+++ script.py
@@ -1,10 +1,18 @@
-# #Create the independent data set
-# for day in df_days:
-#     days.append([int(day.split('-')[4])])
-# #Create the dependent data set (adj close prices)
-# for adj_close_price in df_adj_close:
-#     adj_close_prices.append( float(adj_close_price))
-from pandas._libs.tslibs.timestamps import Timestamp
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-time = Timestamp()
-print(time)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root.val == p.val or root.val == q.val:
+            return root
+        lt, rt = self.lowestCommonAncestor(root.left, p, q), self.lowestCommonAncestor(root.right, p, q)
+        if lt and rt:
+            return root
+        else:
+            return lt if lt else rt
