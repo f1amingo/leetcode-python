0a2c9f3abc6db3742b4d5252526d3c89343bb9f7
flamingo <zsjperiod@foxmail.com>
2021-01-03 16:04:36 +0800

    239. 滑动窗口最大值

--- script.py
+++ script.py
@@ -1,22 +1,67 @@
+import collections
 from typing import List
 
 
-# 两朵花不能相邻
 class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if n == 0:
-            return True
-        size = len(flowerbed) + 2
-        flowerbed = [0] + flowerbed + [0]
-        for i in range(1, size - 1):
-            if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
-                flowerbed[i] = 1
-                n -= 1
-                if n <= 0:
-                    return True
-        return False
+    # 双端队列
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        # 保存下标，下标隐含了窗口大小，用来判断窗口越界
+        queue = collections.deque()
+        # 初始化
+        for i in range(k - 1):
+            # 等于不影响
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            # 当前元素一定在队列中
+            queue.append(i)
+        ans = []
+        for i in range(k - 1, n):
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            while queue and queue[0] <= i - k:
+                queue.popleft()
+            queue.append(i)
+            ans.append(nums[queue[0]])
+        return ans
+
+    # 前缀和
+    # def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    #     n = len(nums)
+    #     to_right, to_left = [0] * n, [0] * n
+    #     this_max = float('-inf')
+    #     for i in range(n):
+    #         if i % k == 0:
+    #             this_max = nums[i]
+    #         else:
+    #             this_max = max(this_max, nums[i])
+    #         to_right[i] = this_max
+    #     this_max = float('-inf')
+    #     for i in range(n - 1, -1, -1):
+    #         if (i + 1) % k == 0:
+    #             this_max = nums[i]
+    #         else:
+    #             this_max = max(this_max, nums[i])
+    #         to_left[i] = this_max
+    #     ans = []
+    #     for i in range(n - k + 1):
+    #         ans.append(max(to_left[i], to_right[i + k - 1]))
+    #     return ans
+
+    # 更简洁的暴力
+    # def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    #     return [max(nums[i:i + k]) for i in range(len(nums) - k + 1)]
+
+    # 暴力超时
+    # def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    #     ans = []
+    #     for i in range(len(nums) - k + 1):
+    #         ans.append(max(nums[i:i + k]))
+    #     return ans
 
 
-assert Solution().canPlaceFlowers([1, 0, 0, 0, 1], 1)
-assert not Solution().canPlaceFlowers([1, 0, 0, 0, 1], 2)
-assert Solution().canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 0)
+assert Solution().maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3) == [3, 3, 5, 5, 6, 7]
+assert Solution().maxSlidingWindow([1], 1) == [1]
+assert Solution().maxSlidingWindow([1, -1], 1) == [1, -1]
+assert Solution().maxSlidingWindow([9, 11], 2) == [11]
+assert Solution().maxSlidingWindow([4, -2], 2) == [4]
--- 滑动窗口/239. 滑动窗口最大值.py
+++ 滑动窗口/239. 滑动窗口最大值.py
@@ -1,7 +1,31 @@
+import collections
 from typing import List
 
 
 class Solution:
+
+    # 双端队列
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        # 保存下标，下标隐含了窗口大小，用来判断窗口越界
+        queue = collections.deque()
+        # 初始化
+        for i in range(k - 1):
+            # 等于不影响
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            # 当前元素一定在队列中
+            queue.append(i)
+        ans = []
+        for i in range(k - 1, n):
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            while queue and queue[0] <= i - k:
+                queue.popleft()
+            queue.append(i)
+            ans.append(nums[queue[0]])
+        return ans
+
     # 奇技淫巧 leftMax + rightMax
     # def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
     #     to_right, to_left = [0] * len(nums), [0] * len(nums)
@@ -18,26 +42,6 @@ class Solution:
     #         low = high - k + 1
     #         ans[low] = max(to_right[high], to_left[low])
     #     return ans
-
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        if n * k == 0:
-            return []
-        if k == 1:
-            return nums
-
-        queue = []
-        ans = []
-        for i in range(n):
-            while queue and nums[queue[-1]] < nums[i]:
-                queue.pop()
-            queue.append(i)
-            if i < k - 1:
-                continue
-            while queue[0] < i - k + 1:
-                queue.pop(0)
-            ans.append(nums[queue[0]])
-        return ans
 
 
 nums = [7, 2, 4]
