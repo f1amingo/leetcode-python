93ea15fc12e45dec5d112b56b5571367ca60a741
flamingo <zsjperiod@foxmail.com>
2021-01-03 14:49:36 +0800

    605. 种花问题

--- 数组 Array/605. 种花问题.py
+++ 数组 Array/605. 种花问题.py
@@ -6,9 +6,8 @@ class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         if n == 0:
             return True
-        size = len(flowerbed) + 2
         flowerbed = [0] + flowerbed + [0]
-        for i in range(1, size - 1):
+        for i in range(1, len(flowerbed) - 1):
             if flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
                 flowerbed[i] = 1
                 n -= 1
