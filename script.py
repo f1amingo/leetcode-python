98659d7007fbeb50f2d863b0a6373a7c785cba65
flamingo <zsjperiod@foxmail.com>
2021-01-04 15:55:10 +0800

    50. Pow(x, n)

--- script.py
+++ script.py
@@ -1,30 +1,19 @@
-from typing import List
-
-
-# n x n矩阵
-# 每行每列升序
-# 第k小的元素
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        # 小于mid的元素数量
-        def check(mid: int):
-            i, j = n - 1, 0
-            count = 0
-            while i >= 0 and j < n:
-                if matrix[i][j] <= mid:
-                    count += i + 1
-                    j += 1
-                else:
-                    i -= 1
-            # 如果count大于k，说明mid过大了，需要减小
-            return count >= k
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0 or x == 1:
+            return x
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        half, mod = divmod(n, 2)
+        ans = self.myPow(x, half)
+        ans = ans * ans
+        if mod == 1:
+            ans *= x
+        return ans
 
-        n = len(matrix)
-        lo, hi = matrix[0][0], matrix[-1][-1]
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if check(mid):
-                hi = mid
-            else:
-                lo = mid + 1
-        return lo
+
+assert Solution().myPow(2, 10) == 1024
