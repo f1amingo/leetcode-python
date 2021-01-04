06567c32666d7efd7d51f010381a304df2b5f0ce
flamingo <zsjperiod@foxmail.com>
2021-01-04 15:07:01 +0800

    230. 二叉搜索树中第K小的元素

--- script.py
+++ script.py
@@ -1,17 +1,23 @@
-from typing import List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from util.ZTree import TreeNode
 
 
+# BST中的第k小元素
+# 中序遍历升序，中序遍历的第k个节点值
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        lo, hi = 0, len(nums) - 1
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if nums[mid] < nums[mid + 1]:
-                lo = mid + 1
-            else:
-                hi = mid
-        return lo
-
-
-assert Solution().findPeakElement([1, 2, 3, 1]) == 2
-assert Solution().findPeakElement([1, 2, 1, 3, 5, 6, 4]) == 5
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        cur, stk = root, []
+        while cur or stk:
+            while cur:
+                stk.append(cur)
+                cur = cur.left
+            cur = stk.pop()
+            k -= 1
+            if k == 0:
+                return cur.val
+            cur = cur.right
--- 树 Tree/230. 二叉搜索树中第K小的元素.py
+++ 树 Tree/230. 二叉搜索树中第K小的元素.py
@@ -9,22 +9,36 @@ from util.ZTree import *
 
 
 class Solution:
+    # 迭代
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        seq, target = 1, None
+        cur, stk = root, []
+        while cur or stk:
+            while cur:
+                stk.append(cur)
+                cur = cur.left
+            cur = stk.pop()
+            k -= 1
+            if k == 0:
+                return cur.val
+            cur = cur.right
 
-        def in_order(node: TreeNode):
-            if node:
-                in_order(node.left)
-                nonlocal seq, target
-                if not target and seq == k:
-                    target = node.val
-                if target:
-                    return
-                seq += 1
-                in_order(node.right)
-
-        in_order(root)
-        return target
+    # 递归
+    # def kthSmallest(self, root: TreeNode, k: int) -> int:
+    #     seq, target = 1, None
+    #
+    #     def in_order(node: TreeNode):
+    #         if node:
+    #             in_order(node.left)
+    #             nonlocal seq, target
+    #             if not target and seq == k:
+    #                 target = node.val
+    #             if target:
+    #                 return
+    #             seq += 1
+    #             in_order(node.right)
+    #
+    #     in_order(root)
+    #     return target
 
 
 roo = buildTreeFromList([3, 1, 4, None, 2])
