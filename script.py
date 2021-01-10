82de910af3be7748c023a8abcf4c89388793df19
flamingo <zsjperiod@foxmail.com>
2021-01-10 12:55:30 +0800

    436. 寻找右区间

--- script.py
+++ script.py
@@ -2,14 +2,60 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        s1, s2, s3, s4 = float('-inf'), 0, float('-inf'), 0
-        for p in prices:
-            s1 = max(s1, -p)
-            s2 = max(s2, s1 + p)
-            s3 = max(s3, s2 - p)
-            s4 = max(s4, s3 + p)
-        return max(s2, s4)
+
+    # 排序+二分
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        n = len(intervals)
+        index_map = {}
+        for i in range(n):
+            index_map[tuple(intervals[i])] = i
+        intervals.sort()
+        ans = [-1] * n
+        for i in range(n - 1):
+            lo, hi = i + 1, n - 1
+            while lo < hi:
+                mid = (lo + hi) >> 1
+                if intervals[mid][0] >= intervals[i][1]:
+                    hi = mid
+                else:
+                    lo = mid + 1
+            if intervals[lo][0] >= intervals[i][1]:
+                ans[index_map[tuple(intervals[i])]] = index_map[tuple(intervals[lo])]
+        return ans
+
+    # 哈希表+排序+扫描
+    # def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+    #     n = len(intervals)
+    #     index_map = {}
+    #     for i in range(n):
+    #         index_map[tuple(intervals[i])] = i
+    #     intervals.sort()
+    #     ans = [-1] * n
+    #     for i in range(n - 1):
+    #         for j in range(i + 1, n):
+    #             if intervals[j][0] >= intervals[i][1]:
+    #                 ans[index_map[tuple(intervals[i])]] = index_map[tuple(intervals[j])]
+    #                 break
+    #     return ans
+
+    # 暴力超时
+    # def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+    #     n = len(intervals)
+    #     ans = [-1] * n
+    #     for i in range(n):
+    #         lt, rt = float('inf'), float('inf')
+    #         idx = -1
+    #         for j in range(n):
+    #             if i == j:
+    #                 continue
+    #             if intervals[i][1] <= intervals[j][0] < lt:
+    #                 lt, rt = intervals[j]
+    #                 idx = j
+    #         ans[i] = idx
+    #     return ans
 
 
-assert Solution().maxProfit([3, 3, 5, 0, 0, 3, 1, 4]) == 6
+assert Solution().findRightInterval([[1, 12], [2, 9], [3, 10], [13, 14], [15, 16], [16, 17]]) == [3, 3, 3, 4, 5, -1]
+assert Solution().findRightInterval([[3, 4], [2, 3], [1, 2]]) == [-1, 0, 1]
+assert Solution().findRightInterval([[1, 2]]) == [-1]
+assert Solution().findRightInterval([[1, 4], [2, 3], [3, 4]]) == [-1, 2, -1]
