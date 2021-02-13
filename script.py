76fdae49dc593646a66a05a169876ecace237b3e
flamingo <zsjperiod@foxmail.com>
2021-02-13 16:30:13 +0800

    二叉树的非递归遍历

--- script.py
+++ script.py
@@ -1,33 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 from typing import List
+
+from util.ZTree import TreeNode
 
 
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        # heap：要排序的堆
-        # root：要调整的节点
-        # N: 当前数组大小
-        def sink(heap, k, N):
-            # 左孩子不越界
-            while 2 * k + 1 < N:
-                j = 2 * k + 1  # 左孩子
-                # 找到左右孩子中的最大节点
-                if j + 1 < N and heap[j + 1] > heap[j]:
-                    j += 1
-                # 根节点不小于左右子树，满足堆的要求
-                if heap[k] >= heap[j]:
-                    break
-                heap[k], heap[j] = heap[j], heap[k]
-                k = j
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        def dfs(node: TreeNode):
+            if not node:
+                return
+            ans.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
 
-        # 建堆，从第一个非叶子结点开始
-        for i in range((len(nums) - 1) >> 1, -1, -1):
-            sink(nums, i, len(nums))
-
-        # 堆排序，把堆顶和最后一个元素交换
-        for i in range(len(nums) - 1, -1, -1):
-            nums[i], nums[0] = nums[0], nums[i]
-            sink(nums, 0, i)
-        return nums
-
-
-assert Solution().sortArray([5, 2, 3, 1]) == [1, 2, 3, 5]
+        ans = []
+        dfs(root)
+        return ans
