49cda8a9936e0981f6d9bd34b62d2851385a340e
flamingo <zsjperiod@foxmail.com>
2021-02-16 20:32:34 +0800

    450. 删除二叉搜索树中的节点

--- script.py
+++ script.py
@@ -1,23 +1,35 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-from typing import List
-
-from util.ZTree import TreeNode
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
-    def preorderTraversal(self, root: TreeNode) -> List[int]:
-        def dfs(node: TreeNode):
-            if not node:
-                return
-            ans.append(node.val)
-            dfs(node.left)
-            dfs(node.right)
-
-        ans = []
-        dfs(root)
-        return ans
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+        if key < root.val:
+            # 左子树中删除
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            # 右子树中删除
+            root.right = self.deleteNode(root.right, key)
+        else:
+            # 删除当前节点
+            if not root.left:
+                # 没有左子树，返回右子树
+                root = root.right
+            elif not root.right:
+                # 没有右子树，返回左子树
+                root = root.left
+            else:
+                # 左右子树都存在，右子树作为新的根
+                # 左子树挂到右子树中最小节点上（作为最小节点的左子树）
+                min_node = root.right
+                while min_node.left:
+                    min_node = min_node.left
+                min_node.left = root.left
+                root = root.right
+        return root
--- 树 Tree/450. 删除二叉搜索树中的节点.py
+++ 树 Tree/450. 删除二叉搜索树中的节点.py
@@ -10,34 +10,61 @@ from util.ZTree import *
 
 class Solution:
     def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
-        def successor(r):
-            r = r.right
-            while r.left:
-                r = r.left
-            return r.val
-
-        def predecessor(r):
-            r = r.left
-            while r.right:
-                r = r.right
-            return r.val
-
         if not root:
-            return root
+            return None
         if key < root.val:
+            # 左子树中删除
             root.left = self.deleteNode(root.left, key)
         elif key > root.val:
+            # 右子树中删除
             root.right = self.deleteNode(root.right, key)
         else:
-            if not root.left and not root.right:
-                root = None
-            elif root.left:
-                root.val = predecessor(root)
-                root.left = self.deleteNode(root.left, root.val)
+            # 删除当前节点
+            if not root.left:
+                # 没有左子树，返回右子树
+                root = root.right
+            elif not root.right:
+                # 没有右子树，返回左子树
+                root = root.left
             else:
-                root.val = successor(root)
-                root.right = self.deleteNode(root.right, root.val)
+                # 左右子树都存在，右子树作为新的根
+                # 左子树挂到右子树中最小节点上（作为最小节点的左子树）
+                min_node = root.right
+                while min_node.left:
+                    min_node = min_node.left
+                min_node.left = root.left
+                root = root.right
         return root
+
+    # def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+    #     def successor(r):
+    #         r = r.right
+    #         while r.left:
+    #             r = r.left
+    #         return r.val
+    #
+    #     def predecessor(r):
+    #         r = r.left
+    #         while r.right:
+    #             r = r.right
+    #         return r.val
+    #
+    #     if not root:
+    #         return root
+    #     if key < root.val:
+    #         root.left = self.deleteNode(root.left, key)
+    #     elif key > root.val:
+    #         root.right = self.deleteNode(root.right, key)
+    #     else:
+    #         if not root.left and not root.right:
+    #             root = None
+    #         elif root.left:
+    #             root.val = predecessor(root)
+    #             root.left = self.deleteNode(root.left, root.val)
+    #         else:
+    #             root.val = successor(root)
+    #             root.right = self.deleteNode(root.right, root.val)
+    #     return root
 
 
 roo = buildTreeFromList([5, 3, 6, 2, 4, None, 7])
