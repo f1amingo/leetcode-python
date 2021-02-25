32ff7c9f3443a81b9b341d13d2cb8143c0a29dcb
flamingo <zsjperiod@foxmail.com>
2021-02-25 20:09:25 +0800

    18. 四数之和

--- script.py
+++ script.py
@@ -1,40 +1,35 @@
 from typing import List
 
 
-# 所有三元组，并且不重复
 class Solution:
-
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
-        if n < 3:
+        if n < 4:
             return []
         nums.sort()
-        ans = []
-        for i in range(n - 2):
-            # 优化
-            if nums[i] > 0:
-                break
-            # 避免重复
+        res = []
+        for i in range(n - 3):
             if i > 0 and nums[i] == nums[i - 1]:
+                # 下一个不同的i
                 continue
-            lt, rt = i + 1, n - 1
-            while lt < rt:
-                this_sum = nums[i] + nums[lt] + nums[rt]
-                if this_sum == 0:
-                    ans.append([nums[i], nums[lt], nums[rt]])
-                if this_sum <= 0:
-                    # 小了，lt右移
-                    while lt < rt and nums[lt] == nums[lt + 1]:
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    # 下一个不同的j
+                    continue
+                lt, rt = j + 1, n - 1
+                while lt < rt:
+                    this_sum = nums[i] + nums[j] + nums[lt] + nums[rt]
+                    if this_sum == target:
+                        res.append([nums[i], nums[j], nums[lt], nums[rt]])
+                    if this_sum <= target:
                         lt += 1
-                    lt += 1
-                if this_sum >= 0:
-                    while lt < rt and nums[rt] == nums[rt - 1]:
+                        while lt < rt and nums[lt] == nums[lt - 1]:
+                            lt += 1
+                    if this_sum >= target:
                         rt -= 1
-                    rt -= 1
-        return ans
+                        while lt < rt and nums[rt] == nums[rt + 1]:
+                            rt -= 1
+        return res
 
 
-assert Solution().threeSum([0, 0, 0, 0]) == [[0, 0, 0]]
-assert Solution().threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
-assert Solution().threeSum([]) == []
-assert Solution().threeSum([0]) == []
+print(Solution().fourSum([1, 0, -1, 0, -2, 2], 0))
