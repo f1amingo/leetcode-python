52230ab149a2d23a59fac0c5394626b72afa208e
flamingo <zsjperiod@foxmail.com>
2021-03-02 21:15:33 +0800

    129. 求根到叶子节点数字之和

--- script.py
+++ script.py
@@ -1,34 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-from util.ZList import *
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from util.ZTree import TreeNode
 
 
 class Solution:
-    def rotateRight(self, head: ListNode, k: int) -> ListNode:
-        if not head or not head.next or k == 0:
-            return head
-        size = 0
-        ptr = head
-        while ptr:
-            size += 1
-            ptr = ptr.next
-        k = k % size
-        if k == 0:
-            return head
-        slow = fast = head
-        for _ in range(k):
-            fast = fast.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next
-        new_head = slow.next
-        slow.next = None
-        fast.next = head
-        return new_head
+    def sumNumbers(self, root: TreeNode) -> int:
+        def dfs(node: TreeNode, t: int):
+            if not node:
+                return
+            t = 10 * t + node.val
+            if not node.left and not node.right:
+                nonlocal ans
+                ans += t
+            dfs(node.left, t)
+            dfs(node.right, t)
 
-
-A = toLinkedList([1, 2, 3, 4, 5])
-printLinkedList(Solution().rotateRight(A, 2))
+        ans = 0
+        dfs(root, 0)
+        return ans
