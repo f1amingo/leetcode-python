c362eb56e581b9a8f33204352b51c43dfe4e7a90
flamingo <zsjperiod@foxmail.com>
2021-03-04 23:02:29 +0800

    148. 排序链表

--- script.py
+++ script.py
@@ -1,26 +1,44 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.left = left
-#         self.right = right
-from typing import List
-
-from util.ZTree import TreeNode
+#         self.next = next
+from util.ZList import *
 
 
 class Solution:
-    def postorderTraversal(self, root: TreeNode) -> List[int]:
-        if not root:
-            return []
-        stk = [root]
-        ans = []
-        while stk:
-            cur = stk.pop()
-            ans.append(cur.val)
-            if cur.left:
-                stk.append(cur.left)
-            if cur.right:
-                stk.append(cur.right)
-        ans.reverse()
-        return ans
+    def sortList(self, head: ListNode) -> ListNode:
+        def merge(a, b) -> ListNode:
+            dummy = ptr = ListNode(-1)
+            while a or b:
+                if not a:
+                    ptr.next = b
+                    b = b.next
+                elif not b:
+                    ptr.next = a
+                    a = a.next
+                else:
+                    if a.val <= b.val:
+                        ptr.next = a
+                        a = a.next
+                    else:
+                        ptr.next = b
+                        b = b.next
+                ptr = ptr.next
+            return dummy.next
+
+        if not head or not head.next:
+            return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        mid = slow.next
+        slow.next = None
+        h1 = self.sortList(head)
+        h2 = self.sortList(mid)
+        return merge(h1, h2)
+
+
+h = toLinkedList([4, 2, 1, 3])
+printLinkedList(Solution().sortList(h))
