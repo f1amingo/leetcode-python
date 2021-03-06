b2a320ac2c78df9ab338e22206eb0c504cb9c9ac
flamingo <zsjperiod@foxmail.com>
2021-03-06 21:21:59 +0800

    153. 寻找旋转排序数组中的最小值

--- script.py
+++ script.py
@@ -2,28 +2,20 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        if not nums:
-            return False
+    def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
         lo, hi = 0, len(nums) - 1
         while lo < hi:
             mid = (lo + hi) // 2
-            if nums[lo] < nums[mid]:
-                if nums[lo] <= target <= nums[mid]:
-                    hi = mid
-                else:
-                    lo = mid + 1
-            elif nums[mid] < nums[hi]:
-                if nums[mid] < target <= nums[hi]:
-                    lo = mid + 1
-                else:
-                    hi = mid
-            else:
-                if nums[lo] == target:
-                    return True
-                lo += 1
-        return True if nums[lo] == target else False
+            if nums[lo] > nums[mid]:  # 前半无序
+                hi = mid
+            elif nums[mid] > nums[hi]:  # 前半有序
+                lo = mid + 1
+            else:  # 前后都有序
+                return min(nums[lo], nums[mid + 1])
+        return nums[lo]
 
 
-assert Solution().search([2, 5, 6, 0, 0, 1, 2], 0)
-assert not Solution().search([2, 5, 6, 0, 0, 1, 2], 3)
+assert Solution().findMin([3, 4, 5, 1, 2]) == 1
+assert Solution().findMin([4, 5, 6, 7, 0, 1, 2]) == 0
+assert Solution().findMin([1]) == 1
--- 数组 Array/二分查找/153. 寻找旋转排序数组中的最小值.py
+++ 数组 Array/二分查找/153. 寻找旋转排序数组中的最小值.py
@@ -7,24 +7,35 @@ from typing import List
 # 在5上旋转：[1,2,3,4,5] (不变)
 # 找出最小元素
 class Solution:
-    # review
     def findMin(self, nums: List[int]) -> int:
-        if not nums:
-            return
         lo, hi = 0, len(nums) - 1
         while lo < hi:
-            # 整体有序lo即为最小
-            if nums[lo] <= nums[hi]:
-                return nums[lo]
             mid = (lo + hi) // 2
-            # 整体无序
-            if nums[lo] <= nums[mid]:
-                # 前半有序，后半无序，最小值在后半
-                lo = mid + 1
-            else:
-                # 前半无序，后半有序，最小值可能是mid，也可能在前半
+            if nums[lo] > nums[mid]:  # 前半无序
                 hi = mid
+            elif nums[mid] > nums[hi]:  # 前半有序
+                lo = mid + 1
+            else:  # 前后都有序
+                return min(nums[lo], nums[mid + 1])
         return nums[lo]
+
+    # def findMin(self, nums: List[int]) -> int:
+    #     if not nums:
+    #         return
+    #     lo, hi = 0, len(nums) - 1
+    #     while lo < hi:
+    #         # 整体有序lo即为最小
+    #         if nums[lo] <= nums[hi]:
+    #             return nums[lo]
+    #         mid = (lo + hi) // 2
+    #         # 整体无序
+    #         if nums[lo] <= nums[mid]:
+    #             # 前半有序，后半无序，最小值在后半
+    #             lo = mid + 1
+    #         else:
+    #             # 前半无序，后半有序，最小值可能是mid，也可能在前半
+    #             hi = mid
+    #     return nums[lo]
 
     # def findMin(self, nums: List[int]) -> int:
     #     if not nums:
