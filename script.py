129224a32fd5204cff72b26c464b79a51ffb3756
flamingo <zsjperiod@foxmail.com>
2021-03-09 20:52:49 +0800

    123. 买卖股票的最佳时机 III

--- script.py
+++ script.py
@@ -3,12 +3,15 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        hold, sell = float('-inf'), 0
+        a, b, c, d = float('-inf'), 0, float('-inf'), 0
         for p in prices:
-            hold0 = sell - p
-            sell0 = hold + p
-            sell, hold = max(sell, sell0), max(hold, hold0)
-        return sell
+            a0 = max(a, -p)
+            b0 = max(b, a + p)
+            c0 = max(c, b - p)
+            d0 = max(d, c + p)
+            a, b, c, d = a0, b0, c0, d0
+        return d
 
 
-assert Solution().maxProfit([7, 1, 5, 3, 6, 4]) == 7
+assert Solution().maxProfit([3, 3, 5, 0, 0, 3, 1, 4]) == 6
+assert Solution().maxProfit([0, 5]) == 5
