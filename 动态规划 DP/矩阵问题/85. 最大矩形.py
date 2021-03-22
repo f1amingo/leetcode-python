2bbcf4cf2231be2a6b508e77c7dad9f26a826816
flamingo <zsjperiod@foxmail.com>
2021-03-22 18:58:19 +0800

    85. 最大矩形

--- 动态规划 DP/矩阵问题/85. 最大矩形.py
+++ 动态规划 DP/矩阵问题/85. 最大矩形.py
@@ -7,14 +7,17 @@ class Solution:
             return 0
         m, n = len(matrix), len(matrix[0])
         ans = 0
+        # (i, j)向左连续1的数量
         dp = [[0] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == '1':
                     dp[i][j] = dp[max(i - 1, 0)][j] + 1
+                    # 高度为1
                     ans = max(ans, dp[i][j])
                     x, y = i, j
                     width = dp[i][j]
+                    # 向上遍历可能的宽度
                     while y >= 0 and matrix[x][y] == '1':
                         width = min(width, dp[x][y])
                         ans = max(ans, (j - y + 1) * width)
