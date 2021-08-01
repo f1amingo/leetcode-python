30a7933e585b68cf26eb668ed8834a4afb3eaf3f
靖泉 <vincent.zsj@alibaba-inc.com>
2021-08-01 16:54:54 +0800

    19. Remove Nth Node From End of List

--- script.py
+++ script.py
@@ -1,24 +1,20 @@
-from typing import List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from util.ZList import ListNode
 
 
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        # 确定start位置的元素
-        def dfs(start: int):
-            if start == n:
-                ans.append(nums.copy())
-                return
-            for i in range(start, n):
-                nums[i], nums[start] = nums[start], nums[i]
-                # dfs(i + 1)
-                # 下一轮应该确定start + 1而不是i
-                dfs(start + 1)
-                nums[i], nums[start] = nums[start], nums[i]
-
-        ans = []
-        n = len(nums)
-        dfs(0)
-        return ans
-
-
-print(Solution().permute([1, 2, 3]))
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(-1)
+        dummy.next = head
+        ptr1, ptr2 = dummy, head
+        for i in range(n):
+            ptr2 = ptr2.next
+        while ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+        ptr1.next = ptr1.next.next
+        return dummy.next
