e212e7cd1bb0e30d5f48a11d34080a84005a55e7
靖泉 <vincent.zsj@alibaba-inc.com>
2021-08-16 11:20:43 +0800

    1143. Longest Common Subsequence

--- script.py
+++ script.py
@@ -1,31 +1,15 @@
-from typing import List
-
-
-# 最大的数，整个数组降序排列
-# 降序大，升序小
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        # 倒序第一段升序左端点
-        m = n - 2
-        while m >= 0 and nums[m] >= nums[m + 1]:
-            m -= 1
-        if m != -1:
-            # 交换，倒序第一个大于A[m]的值
-            k = n - 1
-            while k > m and nums[k] <= nums[m]:
-                k -= 1
-            nums[k], nums[m] = nums[m], nums[k]
-        lt, rt = m + 1, n - 1
-        while lt < rt:
-            nums[lt], nums[rt] = nums[rt], nums[lt]
-            lt += 1
-            rt -= 1
-        print(nums)
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        ans = 0
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if text1[j - 1] == text2[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    ans = max(ans, dp[i][j])
+                else:
+                    dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+        return ans
 
-
-Solution().nextPermutation([5, 1, 1])
-Solution().nextPermutation([1, 5, 1])
+print(Solution().longestCommonSubsequence("abcde", "ace"))
